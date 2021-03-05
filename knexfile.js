0dba4f8954a66f3208b10c7075a4e4fe8b790c22
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-05 11:57:39 -0700

    moved into config folder

--- knexfile.js
+++
@@ -1,8 +0,0 @@
-// Update with your config settings.
-
-module.exports = {
-  development: {
-    client: "pg",
-    connection: "postgres:///done-dev" /** 3 slashes indicates localhost */,
-  },
-};
