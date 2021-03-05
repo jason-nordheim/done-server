2cbafcf3a1027f54ff135fb2302bdd861505c3e6
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-05 11:58:15 -0700

    applied body-parser middleware and corrected cors middlware implementation

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
-const { request, Router } = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const express = require("express");
 const { UriNotFoundError } = require("./errors/UriNotFoundError");
 const { winstonLogger, winstonErrorLogger } = require("./middleware/winston");
+
+const MODE = process.env.NODE_ENV || "development";
+const PORT = process.env.NODE_ENV || 4000;
 
 const app = express();
 
 (function setup() {
   // pre-router application
-  app.use(express.bodyParser());
   app.use(cors());
+  app.use(bodyParser.json());
 
   // logging
   app.use(winstonLogger);
@@ -25,10 +29,8 @@ const app = express();
 
   // error logging
   app.use(winstonErrorLogger);
-  app.use(
-    express.errorLogger({
-      dumpExceptions: true,
-      showStack: true,
-    })
-  );
 })();
+
+app.listen(PORT, () => {
+  console.info(`Server started on http://localhost:${PORT} (mode: ${MODE})`);
+});
