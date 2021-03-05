1c99f40a7c7c48df02c312ba78c7b334c7ec2e5a
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-05 16:18:52 -0700

    commented out http method logging for now

--- src/middleware/winston.js
+++ src/middleware/winston.js
@@ -26,7 +26,7 @@ const winstonLogger = expressWinston.logger({
   ], // everything
   responseWhitelist: ["body"], // this populates the `res.body` so we can get the response size (not required)
   meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+ //msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
   expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
   colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
 });
