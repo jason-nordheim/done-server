c49c63a2b91f9d6d7a8f2805eff93d3fd8d89265
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-05 13:34:21 -0700

    added configuration for migration and seed directories

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -1,8 +1,14 @@
 // Update with your config settings.
-
 module.exports = {
   development: {
     client: "pg",
     connection: "postgres:///done-dev" /** 3 slashes indicates localhost */,
+
+    migrations: {
+      directory: __dirname + "/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/seeds",
+    },
   },
 };
