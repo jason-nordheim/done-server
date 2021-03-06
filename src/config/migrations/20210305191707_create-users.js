90708be81330e9e753ed194a8b2add3b5f76df24
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-05 17:34:16 -0700

    updated user table migration to make the password digest not nullable

--- src/config/migrations/20210305191707_create-users.js
+++ src/config/migrations/20210305191707_create-users.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
     t.timestamps(true, true);
     t.string("username", 50).notNullable().unique();
     t.string("email", 255).notNullable().unique();
-    t.string("password_digest");
+    t.string("password_digest").notNullable();
     t.string("first_name", 100);
     t.string("last_name", 100);
   });
